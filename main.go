e4e7cd66da40e8f685620aa693debf2e051e8b55
riuzaki16 <riuzakii16@gmail.com>
2025-02-06 00:45:25 +0300

    bookmarks app without exit

--- main.go
+++ main.go
@@ -2,18 +2,63 @@ package main
 
 import "fmt"
 
-func main() {
-	m := map[string]string {
-		"PurpleSchool": "purpleschool.ru",
 
+func main() {
+	bookmarks := map[string]string{}
+	fmt.Println("Приложение для закладок")
+	for {
+		variant := getMenu()
+		switch variant {
+		case 1:
+			printBookmarks(bookmarks)
+		case 2:
+			bookmarks = addBookmarks(bookmarks)
+		case 3:
+			bookmarks = deleteBookmarks(bookmarks)
+		case 4:
+			break
+		}
 	}
-	fmt.Println(m)
-	fmt.Println(m["PurpleSchool"])
-	m["PurpleSchool"] = "https://purpleschool.ru"
-	fmt.Println(m)
-	m["Google"] = "https://google.com"
-	m["Yandex"] = "https://yandex.ru"
-	fmt.Println(m)
-	delete(m, "Yandex")
-	fmt.Println(m)
+
 }
+
+
+func getMenu() int{
+	var variant int
+	fmt.Println("Выберете вариант")
+	fmt.Println("1. Посмотреть закладки")
+	fmt.Println("2. Добавить закладку")
+	fmt.Println("3. Удалить закладку")
+	fmt.Println("4. Выход")
+	fmt.Scan(&variant)
+	return variant
+}
+
+func printBookmarks(bookmarks map[string]string) {
+	if len(bookmarks) == 0 {
+		fmt.Println("Заметки отсутсвует")
+	}
+	for key, value := range bookmarks {
+		fmt.Println(key, ":", value)
+	}
+}
+
+func addBookmarks(bookmarks map[string]string) map[string]string{
+	var newBookmarkKey string
+	var newBookmarkValue string
+	fmt.Println("Введите название: ")
+	fmt.Scan(&newBookmarkKey)
+	fmt.Println("Введите ссылку: ")
+	fmt.Scan(&newBookmarkValue)
+	bookmarks[newBookmarkKey] = newBookmarkValue
+	return bookmarks
+}
+
+func deleteBookmarks(bookmarks map[string]string) map[string]string{
+	var bookmarkKeyToDelete string 
+	fmt.Print("Введите название: ")
+	fmt.Scan(&bookmarkKeyToDelete)
+	delete(bookmarks, bookmarkKeyToDelete)
+	return bookmarks
+}
+                
